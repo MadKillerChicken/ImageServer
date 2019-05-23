6ad774d576cd73a7e36714b2fbd325a6efdbae1e
Marlon Sardini <msardini@outlook.com>
2019-05-23 22:35:52 +0200

    Added some classes to start with something.

--- ImageServer/ImageServer/Program.cs
+++ ImageServer/ImageServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,5 +14,36 @@ namespace ImageServer
             Console.WriteLine("HELLO");
             Console.ReadLine();
         }
+    }
+
+    internal class ImageEntry : MediaEntry
+    {
+        public override IEnumerable<MediaSubject> Subjects { get => throw new NotImplementedException(); protected set => throw new NotImplementedException(); }
+    }
+
+    internal abstract class MediaEntry
+    {
+        public abstract IEnumerable<MediaSubject> Subjects { get; protected set; }
+
+        public FileInfo Location { get; protected set; } 
+    }
+
+    /// <summary>
+    /// Subject displayed in an image
+    /// </summary>
+    internal class ImageSubject : MediaSubject
+    {
+
+    }
+
+    /// <summary>
+    /// Basic main content of a media entry.
+    /// </summary>
+    internal abstract class MediaSubject
+    {
+        public string Name { get; protected set; }
+
+        public string Description { get; protected set; }
+
     }
 }
